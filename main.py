07b52d0eecf08a4bc41bf21241f15ea8773f22d6
BiuBiuBia <2549639382@qq.com>
2021-03-19 10:03:52 +0800

    update-3.19

--- main.py
+++ main.py
@@ -1,69 +1,93 @@
 # 线程: 线程可以理解成执行代码的分支， 线程是执行对应的代码的， cpu调度线程去执行对应代码
+import sys
 import time
 import threading
+import random
+from selenium.webdriver.firefox.options import Options
+import requests
 from selenium import webdriver
 
 
-def zgyq():
-    opt = webdriver.ChromeOptions()  # 创建浏览器
-    # opt.set_headless()                            #无窗口模式
-    driver = webdriver.Chrome(options=opt)  # 创建浏览器对象
-    driver.get('https://www.baidu.com/')  # 打开网页
-    # driver.maximize_window()                      #最大化窗口  # 加载等待
+def direct_firefox(proxy):
+    profile = webdriver.FirefoxProfile()
+    ip, port = proxy.split(":")
+    port = int(port)
+    settings = {
+        'network.proxy.type': 1,  # 0: 不使用代理；1: 手动配置代理
+        'network.proxy.http': ip,
+        'network.proxy.http_port': port,
+        'network.proxy.ssl': ip,  # https的网站,
+        'network.proxy.ssl_port': port,
+    }
+    # 更新配置文件
+    for key, value in settings.items():
+        profile.set_preference(key, value)
+    profile.update_preferences()
 
-    # driver.find_element_by_xpath("./*//span[@class='bg s_ipt_wr quickdelete-wrap']/input").send_keys(
-    #    "ZGYQ")  # 利用xpath查找元素进行输入文本
-    driver.find_element_by_id('kw').send_keys("ZGYQ")
-
-    # driver.find_element_by_xpath("//span[@class='bg s_btn_wr']/input").click()  # 点击按钮
-    driver.find_element_by_xpath("//input[@value='百度一下']").click()
-    # driver.find_element_by_link_text("ZGYQ_企业商标大全_商标信息查询_爱企查").click()
-    # driver.find_element_by_xpath("//span[@class='bg s_btn_wr']/input[type='submit'][value='百度一下']").click()#候选方法,多条件匹配
-    time.sleep(20)
-
-
-# 唱歌
-def xmu():
-    opt = webdriver.ChromeOptions()  # 创建浏览器
-    # opt.set_headless()                            #无窗口模式
-    driver = webdriver.Chrome(options=opt)  # 创建浏览器对象
-    driver.get('https://www.baidu.com/')  # 打开网页
-    # driver.maximize_window()                      #最大化窗口
-    # 加载等待
-
-    # driver.find_element_by_xpath("./*//span[@class='bg s_ipt_wr quickdelete-wrap']/input").send_keys(
-    #     "魅族")  # 利用xpath查找元素进行输入文本
-    driver.find_element_by_id('kw').send_keys("xmu") #候选方法
-
-    # driver.find_element_by_xpath("//span[@class='bg s_btn_wr']/input").click()  # 点击按钮
-    driver.find_element_by_xpath("//input[@value='百度一下']").click()
-    # driver.find_element_by_xpath("//span[@class='bg s_btn_wr']/input[type='submit'][value='百度一下']").click()#候选方法,多条件匹配
-
-    time.sleep(20)
+    options = Options()
+    firefox = webdriver.Firefox(executable_path='C:/Program Files (x86)/Mozilla Maintenance Service/geckodriver.exe',
+                                firefox_profile=profile, options=options)
+    firefox.get('http://med.ckcest.cn/index.html')
+    event = threading.Event()
+    event.wait(random.uniform(100, 120))
+    firefox.find_element_by_id('searchText').send_keys("医药")
+    firefox.find_element_by_xpath('/html/body/div[1]/div[4]/div/div[2]/div[2]/a').click()
+    event.wait(10)
+    firefox.quit()
 
 
-if __name__ == '__main__':
+def direct_chrome(ip):
+    url = "http://med.ckcest.cn/index.html"
+    proxy = ip
+    chromeOptions = webdriver.ChromeOptions()  # 设置代理
+    chromeOptions.add_argument('--proxy-server={0}'.format(proxy))
+    driver = webdriver.Chrome("C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe",
+                              options=chromeOptions)
+    driver.get(url)
+    event = threading.Event()
+    event.wait(random.uniform(100, 120))
+    driver.find_element_by_id('searchText').send_keys("医药")
+    driver.find_element_by_xpath('/html/body/div[1]/div[4]/div/div[2]/div[2]/a').click()
+    event.wait(10)
+    driver.quit()
 
-    # 获取当前执行代码的线程
-    current_thread = threading.current_thread()
-    print("main:", current_thread)
-    # 获取程序活动线程的列表
-    thread_list = threading.enumerate()
-    # print("111:", thread_list)
-    # 创建跳舞的线程
-    baidu_thread = threading.Thread(target=xmu)
-    ZGYQ_thread = threading.Thread(target=zgyq)
-    # print("sing_thread:", sing_thread)
-    thread_list = threading.enumerate()
-    # 启动线程执行对应的任务
-    baidu_thread.start()
-    thread_list = threading.enumerate()
-    print("222:", thread_list)
-    ZGYQ_thread.start()
-    # 提示:线程执行完成任务以后该线程就会销毁
-    thread_list = threading.enumerate()
-    print("333:", thread_list, len(thread_list))
-    # 扩展-获取活动线程的个数
-    active_count = threading.active_count()
-    print(active_count)
-    # 注意点： 线程之间执行是无序的，由cpu调度决定的
+
+if __name__ == "__main__":
+    from concurrent.futures import ThreadPoolExecutor
+
+    threadPool = ThreadPoolExecutor(max_workers=10, thread_name_prefix="thread_")
+    while True:
+        # 各浏览器数目
+        chrome_num = 1
+        firefox_num = 1
+        # ip总数
+        all_num = chrome_num + firefox_num
+        ip_url = "http://api.wandoudl.com/api/ip?app_key=b342b861c460906ba70b171b7a758b0c&pack=0&num=" + str(
+            all_num) + "&xy=1&type=1&lb=\r\n&mr=1&area_id= "
+        ip = requests.get(ip_url).text
+        ip_list = ip.split()
+        # 记录各浏览器循环次数
+        firefox_flag = firefox_num
+        chrome_flag = chrome_num
+
+        for i in range(chrome_num + firefox_num):
+            if chrome_flag > 0:
+                if random.randint(1, 2) == 1:
+                    threadPool.submit(direct_chrome, ip_list[i])
+                else:
+                    print("chrome_click")
+                chrome_flag = chrome_flag - 1
+                i = i + 1
+
+            if firefox_flag > 0:
+                if random.randint(1, 2) == 1:
+                    threadPool.submit(direct_firefox, ip_list[i])
+                else:
+                    print("firefox_click")
+                firefox_flag = firefox_flag - 1
+                i = i + 1
+
+        ip_list = []
+        event = threading.Event()
+        event.wait(random.uniform(130, 150))
+        print("over")
